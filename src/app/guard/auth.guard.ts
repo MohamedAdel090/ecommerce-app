153b79b407456d08f922b36db1dec5059fa5cc7b
Muhammed Adel <mohamedadel@gmail.com>
2024-07-28 12:07:40 +0300

    changes auth guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -8,11 +8,20 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    if (this.isLocalStorageAvailable() && localStorage.getItem('_token')) {
-      // User is logged in
-      return true;
+    // Check if localStorage is available
+    if (this.isLocalStorageAvailable()) {
+      // Check if the user is logged in
+      const isLoggedIn = !!localStorage.getItem('_token');
+      if (isLoggedIn) {
+        // User is logged in, allow access to the route
+        return true;
+      } else {
+        // User is not logged in, redirect to the login page
+        this.router.navigate(['/login']);
+        return false;
+      }
     } else {
-      // User is not logged in, redirect to login page
+      // localStorage is not available, redirect to the login page
       this.router.navigate(['/login']);
       return false;
     }
@@ -20,11 +29,13 @@ export class AuthGuard implements CanActivate {
 
   private isLocalStorageAvailable(): boolean {
     try {
+      // Perform a test operation on localStorage
       const testKey = '__test__';
       localStorage.setItem(testKey, testKey);
       localStorage.removeItem(testKey);
       return true;
     } catch (e) {
+      // If an error occurs, localStorage is not available
       return false;
     }
   }
